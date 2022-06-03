81efcab550a554bd22c4b287c4a7bcf7e20b55e2
nikhilminaj <nikhilkumarpctebca20@gmail.com>
2022-06-03 10:16:20 +0530

    first commit

---
+++ node.js
@@ -0,0 +1,6 @@
+const chd = require('readline-sync')
+var a,b, total
+a = chd.questionInt('Enter the First Number ')
+b = chd.questionInt('Enter the Second Number ')
+total = a + b
+console.log('Total is '+total)
